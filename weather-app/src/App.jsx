6a9f34d59e02907ab826f0a83d1817abf5d82bac
Nipun Raj <nipunraj09@gmail.com>
2024-09-30 21:29:10 -0700

    Initial commit

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,12 @@
+import WeatherApp from './components/Weatherapp'
+import './components/Weatherapp.css'
+
+const App = () => {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  )
+}
+
+export default App
